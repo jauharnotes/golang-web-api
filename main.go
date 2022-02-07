e019958528136233a1d5889df16514ed8a78f3a9
jauharnotes <jauharnotes@gmail.com>
2022-02-07 18:07:56 +0700

    path variable and query string

--- main.go
+++ main.go
@@ -13,6 +13,10 @@ func main() {
 
 	router.GET("/blog", blogHandler)
 
+	router.GET("/books/:id/:title", booksHandler)
+
+	router.GET("/query", queryHandler)
+
 	router.Run(":5000")
 }
 
@@ -29,4 +33,19 @@ func blogHandler(c *gin.Context) {
 		"tanggal":  "06 Februari 2022",
 		"paragraf": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam tristique erat et neque lacinia placerat.",
 	})
+
+}
+
+func booksHandler(c *gin.Context) {
+	id := c.Param("id")
+	title := c.Param("title")
+
+	c.JSON(http.StatusOK, gin.H{"id": id, "title": title})
+}
+
+func queryHandler(c *gin.Context) {
+	title := c.Query("title")
+	terbit := c.Query("terbit")
+
+	c.JSON(http.StatusOK, gin.H{"title": title, "terbit": terbit})
 }
